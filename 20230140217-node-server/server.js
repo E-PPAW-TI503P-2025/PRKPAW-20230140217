e072b08a1b76573807e71c593fa2691e2748e802
Asti22 <uun3461@gmail.com>
2025-10-02 07:11:09 +0700

    first commit

---
+++ 20230140217-node-server/server.js
@@ -0,0 +1,16 @@
+var express = require('express')
+var cors = require('cors')
+var app = express()
+var port = 5000
+
+app.use(cors())
+
+
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello from Node.js Server!' });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
